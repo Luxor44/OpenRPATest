5ca4f1381af7458f6b6efa9c1a679afb904cb951
Luxor44 <92293868+Luxor44@users.noreply.github.com>
2024-12-19 10:10:44 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -18,7 +18,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+logging.info(f'we are starting')
 BrowserUrl = "google.com"
 Chromeoptions = self.webdriver.ChromeOptions() 
 DownloadOptions = {
